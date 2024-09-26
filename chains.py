a9f09042748aef1d39df98b3626113cdd3145b7e
Veera Prathap <105205233+veeraprathp@users.noreply.github.com>
2024-09-26 02:42:53 +0200

    Read to go

---
+++ chains.py
@@ -0,0 +1,54 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Veera Prathap, a Master's student in Artificial Intelligence at Brandenburg University of Technology Cottbus-Senftenberg. Your expertise spans Natural Language Processing (NLP), transformer models like BERT and GPT-2, as well as practical experience with Gensim for Named Entity Recognition (NER). You are currently looking for Machine Learning/AI positions that align with your skills and knowledge. Over your academic journey, you have worked on multiple projects, including a machine learning pipeline combining BERT for document retrieval and GPT-2 for response generation, alongside other classification algorithms for various use cases such as cyberbullying detection and crop recommendation models.
+            Your job is to write to potential employers, showcasing your technical expertise, your hands-on experience with transformer models, and your dedication to applying advanced AI techniques. Mention relevant projects, academic achievements, and emphasize your practical approach to solving real-world problems. Include examples that best highlight your potential contributions to the field. 
+                Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
